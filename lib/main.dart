1055c568e4cbaff40b22f8a944cf8d4993b8ae5e
KodaiIkari <Ikari.Koudai@gmail.com>
2024-10-24 23:02:21 +0900

    メイン画面タイトル編国

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter デモ ログイン画面'),
+      home: const MyHomePage(title: 'Flutter デモ API呼び出し画面'),
     );
   }
 }
