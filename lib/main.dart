e46d3b7903bccafb648b52cc5409584739936c94
KodaiIkari <Ikari.Koudai@gmail.com>
2024-10-27 19:03:24 +0900

    WebViewの表示・非表示を切り替えるボタンの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isWebView = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +43,21 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(onPressed: () {}, child: const Text('WebViewの表示')),
+            // isWebViewがtrueの場合はWebViewを表示
+            if (isWebView)
+              Container(
+                width: 300,
+                height: 300,
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: const TestWebView(),
+              ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isWebView = !isWebView;
+                  });
+                },
+                child: const Text('WebViewの表示')),
             Padding(
               padding: const EdgeInsets.only(top: 20),
               child: ElevatedButton(
@@ -50,5 +67,26 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+class TestWebView extends StatefulWidget {
+  const TestWebView({super.key});
+
+  @override
+  State<TestWebView> createState() => _TestWebViewState();
+}
+
+class _TestWebViewState extends State<TestWebView> {
+  late WebViewController controller = WebViewController();
+
+  void setWebView(String url) {
+    controller.loadRequest(Uri.parse(url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.loadRequest(Uri.parse('https://www.google.com'));
+    return WebViewWidget(controller: controller);
   }
 }
