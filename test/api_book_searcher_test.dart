6d093a651f1f1d44c9fd82805757868f5eacad93
KodaiIkari <Ikari.Koudai@gmail.com>
2024-12-15 11:57:28 +0900

    API呼び出しテストの追加

--- test/api_book_searcher_test.dart
+++ test/api_book_searcher_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:mockito/annotations.dart';
 
-
 @GenerateNiceMocks([MockSpec<BookApiManager>()])
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -29,30 +28,25 @@ Future<void> main() async {
     test("実際のAPIレスポンスを使った正常系テスト", () {
       dioAdapter.onGet(
           '/volumes',
-          (server) =>
+              (server) =>
               server.reply(200, fixture('google_book_api_example_all.json')),
           queryParameters: {'q': 'success'});
     });
 
     test("テストデータを正しく作成できる事のテスト", () async {
-      dioAdapter.onGet(
-          '/volumes',
-          (server) => server.reply(
-              200,
-              BookSearchResponse(totalItems: 100, items: [
-                BookSearchData(
-                    volumeInfo: BookData(title: "title1", publisher: "出版社1")),
-                BookSearchData(
-                    volumeInfo: BookData(title: "title2", publisher: "出版社2"))
-              ]).toJson()),
+      dioAdapter.onGet('/volumes',
+              (server) => server.reply(200, createSuccessResponse(3).toJson()),
           queryParameters: {'q': 'success'});
 
       final response = await bookApiClient.getBookDataFromApi("success");
-      expect(response.totalItems, 100);
-      expect(response.items[0].volumeInfo?.title, "title1");
-      expect(response.items[0].volumeInfo?.publisher, "出版社1");
-      expect(response.items[1].volumeInfo?.title, "title2");
-      expect(response.items[1].volumeInfo?.publisher, "出版社2");
+      expect(response.totalItems, 3);
+      expect(response.items[0].volumeInfo?.title, "title0");
+      expect(response.items[0].volumeInfo?.publisher, "出版社0");
+      expect(response.items[0].dataLink, "link0");
+      expect(response.items[0].id, "id0");
+
+      expect(response.items[1].volumeInfo?.title, "title1");
+      expect(response.items[1].volumeInfo?.publisher, "出版社1");
     });
 
     test("ステータスコード400が帰ってきたときのエラー", () async {
@@ -65,8 +59,10 @@ Future<void> main() async {
     test("検索結果が0の場合に正しく処理されることのテスト", () async {
       dioAdapter.onGet(
           '/volumes',
-          (server) => server.reply(
-              200, jsonDecode('{"kind": "books#volumes", "totalItems": 0}')),
+              (server) =>
+              server.reply(
+                  200,
+                  jsonDecode('{"kind": "books#volumes", "totalItems": 0}')),
           queryParameters: {'q': 'no_result'});
 
       final response = await bookApiClient.getBookDataFromApi("no_result");
@@ -74,8 +70,32 @@ Future<void> main() async {
       expect(response.items.length, 0);
     });
   });
+
+  group("APIレスポンスの変換", () {
+    test("名前一覧の取得_正常系", () async {
+      dioAdapter.onGet('/volumes',
+              (server) => server.reply(200, createSuccessResponse(3).toJson()),
+          queryParameters: {'q': 'success'});
+
+      final bookApiManager = BookApiManager(bookApiClient);
+      final bookTitles = await bookApiManager.getBoolTitles("success");
+      expect(bookTitles, ["title0", "title1", "title2"]);
+    });
+  });
 }
 
 String fixture(String name) {
   return File('test/stub/$name').readAsStringSync();
+}
+
+/// 正常系のテスト用APIレスポンスを作成
+BookSearchResponse createSuccessResponse(int itemCount, {int? totalItems}) {
+  return BookSearchResponse(
+    totalItems: (totalItems == null) ? itemCount : totalItems,
+    items: List.generate(itemCount, (index) {
+      return BookSearchData(dataLink: "link$index", id: "id$index",
+          volumeInfo: BookData(
+              title: "title$index", publisher: "出版社$index"));
+    }),
+  );
 }
